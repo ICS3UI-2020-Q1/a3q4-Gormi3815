efd95a92a366f9b49a2ad847fc97b76270eb5ce1
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-04 19:07:14 +0000

    will tell you the full version of an integer in between 1-10

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,43 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a positive integer");
+    int number = input.nextInt();
+    switch(number){
+      case 1:
+      System.out.println("one");
+      break;
+      case 2:
+      System.out.println("two");
+      break;
+      case 3:
+      System.out.println("three");
+      break;
+      case 4:
+      System.out.println("four");
+      break;
+      case 5:
+      System.out.println("five");
+      break;
+      case 6:
+      System.out.println("six");
+      break;
+      case 7:
+      System.out.println("seven");
+      break;
+      case 8:
+      System.out.println("eight");
+      break;
+      case 9:
+      System.out.println("nine");
+      break;
+      case 10:
+      System.out.println("ten");
+      break;
+      default:
+      System.out.println("Not in the range.");
+    }
     
   }
 }
